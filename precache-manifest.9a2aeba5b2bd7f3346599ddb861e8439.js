9a2be5f3867ae800e9ce7d6fa1489a66cc0dec34
HuyPV <nhuthuy212507@gmail.com>
2020-01-05 14:16:16 +0700

    Updates

---
+++ precache-manifest.9a2aeba5b2bd7f3346599ddb861e8439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61eab7a67f6d2634954ff82a68f15c57",
+    "url": "/game-minesweeper/index.html"
+  },
+  {
+    "revision": "d2b411d0b5f4726feb8a",
+    "url": "/game-minesweeper/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b50f1c6d573a2489a289",
+    "url": "/game-minesweeper/static/js/2.0faebfbf.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/game-minesweeper/static/js/2.0faebfbf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2b411d0b5f4726feb8a",
+    "url": "/game-minesweeper/static/js/main.fdf78011.chunk.js"
+  },
+  {
+    "revision": "a2ead8830377aa2f58c4",
+    "url": "/game-minesweeper/static/js/runtime-main.bebb65f1.js"
+  }
+]);
